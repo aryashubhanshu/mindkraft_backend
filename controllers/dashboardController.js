7f087661aa0f7de1af77b751cd4ef6fe2fa24f33
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2025-01-22 15:28:52 +0530

    fix: dashboard metrics

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Assessment from "../models/Assessment.js";
 import Invitation from "../models/Invitation.js";
 
@@ -32,17 +33,35 @@ export const getDashboardMetrics = async (req, res, next) => {
 // Get data for charts
 export const getChartData = async (req, res, next) => {
   try {
-    const submissionsByType = await Assessment.aggregate([
-      { $match: { createdBy: req.user.id } },
+    const userId = new mongoose.Types.ObjectId(req.user.id);
+
+    // Count by Type
+    const typesData = await Assessment.aggregate([
+      { $match: { createdBy: userId } }, // Match assessments created by the user
       {
         $group: {
-          _id: "$type",
-          submissions: { $sum: { $size: "$submissions" } },
+          _id: "$type", // Group by the "type" field
+          count: { $sum: 1 }, // Count the number of assessments of each type
         },
       },
     ]);
 
-    res.json({ submissionsByType });
+    // Count by User Group
+    const userGroupsData = await Assessment.aggregate([
+      { $match: { createdBy: userId } }, // Match assessments created by the user
+      { $unwind: "$userGroup" }, // Unwind the array of user groups
+      {
+        $group: {
+          _id: "$userGroup", // Group by each user group value
+          count: { $sum: 1 }, // Count the number of occurrences for each user group
+        },
+      },
+    ]);
+
+    res.json({
+      types: typesData,
+      userGroups: userGroupsData,
+    });
   } catch (error) {
     next(error);
   }
