16799bb136810d389f86697fb8cacc5619fcf0a2
Rahul Vils <rahul.palamarthi@vils.ai>
2025-02-28 11:26:17 +0530

    chore: server log

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get("/", (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on  http://localhost:${PORT}`);
 });
